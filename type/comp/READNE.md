53b969537bdccccbd7e11c02e19a139b9fbd76a7
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-05-12 19:24:57 +0900

    わけわかめ

---
+++ type/comp/READNE.md
@@ -0,0 +1,5 @@
+# 競技プログラムに想定した機能確認
+
+* 標準入力
+    * [参考](https://qiita.com/pco2699/items/aa3a62fa9d0739f57a4a)
+    * 「ts-node」はtypescriptのスクリプトをそのまま実行可能にするライブラリ。通常はjavascriptへの変換が必要
---
+++ type/comp/std/stdinput.ts
@@ -0,0 +1,7 @@
+import * as fs from "fs";
+const input = fs.readFilsSync("/dev/stdin","utf8").split();
+const a = +input[0];
+const b = +input[1];
+console.log(input[0]);
+console.log(input[1]);
+
