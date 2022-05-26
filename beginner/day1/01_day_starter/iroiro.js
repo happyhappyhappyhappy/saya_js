70788e29cb066a295755f7f4bd5fb3172d28202b
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-05-26 18:57:39 +0900

    chenge Javascript in wsl

--- beginner/day1/01_day_starter/iroiro.js
+++ beginner/day1/01_day_starter/iroiro.js
@@ -2,12 +2,13 @@ let firstName = "YAMANAKA"
 let lastName = "Kohji"
 let country = "Japan"
 let city = "Tokyo"
-let age = 48
+let age = 49
 let isMarried = false
 
 const gravity = 9.81
 const boilingPoint = 100
 const PI = 3.14
-let name = "yamanyaka",
+let names = "yamanyaka",
 job = "teacher",
 live = "kita chousen"
+console.log(gravity,boilingPoint,names,job,live)
