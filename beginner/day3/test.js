dc96c02e746efdd8865b39c1fa079ac75b848206
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-05-28 19:09:23 +0900

    練習問題に取り組み始めた。nodeを使えば良いじゃん

---
+++ beginner/day3/test.js
@@ -0,0 +1,17 @@
+console.log("Level1")
+console.log("Problem.1")
+for(let j1=0;j1<11;j1++){
+    console.log(`${j1}`)
+}
+console.log("Problem.2")
+let j2=0
+while(j2 < 11){
+    console.log(`${j2}`)
+    j2 = j2 + 1
+}
+console.log("do while")
+let j3=0
+do{
+    console.log(`${j3}`)
+    j3 = j3+1
+}while(j3<=10)
---
+++ beginner/day3/three.js
@@ -0,0 +1,7 @@
+console.log("2022/05/28")
+let randomNum = Math.random()
+let from0to10Num = randomNum * 11
+console.log(from0to10Num)
+console.log(Math.floor(from0to10Num))
+let a = 2
+let b = 3
